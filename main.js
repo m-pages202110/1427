e695474f93de0b270aef530f5fefc95113fe2cdb
m-pages202110 <93059365+m-pages202110@users.noreply.github.com>
2021-11-06 05:57:55 +0900

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,20 @@ $(function () {
   $(".hamburger").click(function(){
     $("body").toggleClass("menu-open");
   });
-  $(".search").click(function(){
-    $("header").toggleClass("search-open");
+  // $(".search").click(function(){
+  //   $("header").toggleClass("search-open");
+  // });
+
+  $('#mask').click(function() {
+    // openクラスを削除して、メニューを閉じる
+    $('body').removeClass('menu-open');
+  });
+
+  ScrollReveal().reveal('.row', { 
+    duration: 800, // アニメーションの完了にかかる時間
+    viewFactor: 0.2, // 0~1,どれくらい見えたら実行するか
+    reset: true,   // 何回もアニメーション表示するか
+    origin: 'bottom',
+    distance: '20px'
   });
 });
