a8613eea6cddd324f5ed3a0e525e21db6f3e4bec
m-pages202110 <93059365+m-pages202110@users.noreply.github.com>
2021-11-25 10:45:01 +0900

    Update main.js

--- main.js
+++ main.js
@@ -8,6 +8,12 @@ $(function () {
       speed: 400,
       dots: true
   });
+   $('.slider').slick({
+    autoplay:true, // 自動再生を設定
+    autoplaySpeed:5000, // スライド切り替えの時間を設定
+    dots:true, // インジケーターを表示
+    // adaptiveHeight:true
+  });
 
   $(".hamburger").click(function(){
     $("body").toggleClass("menu-open");
